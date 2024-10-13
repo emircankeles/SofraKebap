61a386f8e975e0235295ca4c5fde5053a5852cef
Şahin Emircan KELES <sahinemircankeles@gmail.com>
2024-10-13 11:26:13 +0300

    13.10.2023 devamı

---
+++ WebUI/Dtos/BookingDtos/ResultBookingDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebUI.Dtos.BookingDtos
+{
+    public class ResultBookingDto
+    {
+        public int BookingID { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Mail { get; set; }
+        public int PersonCount { get; set; }
+        public DateTime BookingTime { get; set; }
+    }
+}
